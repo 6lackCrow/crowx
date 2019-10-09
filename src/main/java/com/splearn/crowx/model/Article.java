2eea923559b43d0710e23aa89ae2215463ed0ec0
crowX <1871339825@qq.com>
2019-10-09 09:41:54 +0800

    init

--- src/main/java/com/splearn/crowx/model/Article.java
+++ src/main/java/com/splearn/crowx/model/Article.java
@@ -1,4 +1,27 @@
 package com.splearn.crowx.model;
-
 public class Article {
+
+    /**
+     * id : 311
+     * author : 李老八45554
+     */
+
+    private int id;
+    private String author;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
 }
